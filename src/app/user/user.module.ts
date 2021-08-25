fdd36f41a9454100cf45fe2c291d7915bba14f8c
VamsijKrish <vamsijkrish@gmail.com>
2021-08-26 00:07:28 +0530

    changed modules

--- src/app/user/user.module.ts
+++ src/app/user/user.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { UserRoutingModule } from './user-routing.module';
-
+import { LoginComponent } from './components/auth/login/login.component';
+import { RegisterComponent } from './components/auth/register/register.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    UserRoutingModule
-  ]
+  declarations: [LoginComponent, RegisterComponent],
+  imports: [CommonModule, FormsModule, UserRoutingModule],
 })
-export class UserModule { }
+export class UserModule {}
